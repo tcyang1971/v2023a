c595d5ba5b7b30457d9b5e110c1680167aacf7ff
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-10-26 14:13:50 +0800

    make it better

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from datetime import datetime
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    X = "作者:楊子青<br>"
+    X += "<a href=/mis>資管導論</a><br>"
+    X += "<a href=/today>日期時間</a><br>"
+    X += "<a href=/about>子青網頁</a><br>"
+    return X
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html",datetime = str(now))
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    #app.run()
+    serve(app, host='0.0.0.0', port=80)
+
---
+++ test.py
@@ -0,0 +1,3 @@
+X = input("請輸入您的名字:")
+Y = input("請輸入您的課程:")
+print("早安!{},今天的課程{}介紹Python".format(X,Y))
